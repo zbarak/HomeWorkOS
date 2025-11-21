16a8608c2f9835e70d9094d6b1e049c8f8573f52
Danielz <Danielbrovender@gmail.com>
2025-11-21 16:53:21 +0200

    fixed main

--- tests/src/func.c
+++ tests/src/func.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#include "../header/func.h"
 
-void printddd()
+void shrag()
 {
-    printf("hello daniel!\n");
+    printf("Shrags.\n");
 }
 
-
-void newTestFunc()
-{
-    printf("WOW");
+int split2words(char *words[], char *word_pointer){
+    int i = 0;
+    //word_pointer = line; // marks new line
+    while((*word_pointer == ' ') || (*word_pointer == '\t')){word_pointer++;} //skips space abd \t
+    while((*word_pointer != '\0') && (i < WORDSIZE)){
+        words[i] = word_pointer; //place word into array
+        while((*word_pointer != ' ') && (*word_pointer != '\t') && (*word_pointer != '\0')){word_pointer++;}
+        
+        if(*word_pointer == '\0') {i++; break;}
+        *word_pointer = '\0';
+        word_pointer++;
+        while((*word_pointer == ' ') || (*word_pointer == '\t')) word_pointer++;
+        i++;
+    }
+    //if (words[i] == NULL) i--;
+    //printf("%s\n", line);
+    return i;
 }
