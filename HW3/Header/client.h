2da118a9f5c96e0abc118b2eb5891e75527544ba
zo <zohar.cre@gmail.com>
2025-12-18 14:11:42 +0200

    skelaton of the project

---
+++ HW3/Header/client.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include "../Header/client.h"
+
+#define BUFFER_SIZE 1024
